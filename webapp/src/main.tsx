d3f86f4d8b859e6cb55134db0dbd1d21403f2119
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-06-11 16:23:47 +0530

    feat: implement authentication flow and layout structure with MUI components

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './App.tsx';
+import { theme } from './theme/theme.ts';
+import { ProductProvider } from './context/ProductContext.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <ProductProvider>
+          <App />
+        </ProductProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
